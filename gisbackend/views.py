b533c6e723b8c90ab4b8f889be1093e10686419a
Sandi Rizqi <a.sandi.rizqi@gmail.com>
2023-05-09 09:20:10 +0000

    Update views.py

--- gisbackend/views.py
+++ gisbackend/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse, JsonResponse
 
 
 def TestURL(request):
-    HttpResponse ({"message": "POST Successfully" }, status=201)
+    return HttpResponse ({"message": "POST Successfully" }, status=201)
