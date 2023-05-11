df0b5bc76e34b611621c041be0bd66943b43d0d6
SANDI <sandi@SANDIs-MacBook-Pro.local>
2023-05-11 08:29:01 +0700

    add version

--- gisbackend/views.py
+++ gisbackend/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse, JsonResponse
 
 
 def TestURL(request):
-    return JsonResponse ({"message": "POST Successfully" })
+    return JsonResponse ({"message": "Service is online",
+                          "version": "V1.0" })
