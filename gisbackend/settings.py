3e1adbcac6f198236c4e34fc953c5e2a800ef581
sandi.rizqi <a.sandi.rizqi@gmail.com>
2023-05-26 13:59:01 +0700

    change prod mode

--- gisbackend/settings.py
+++ gisbackend/settings.py
@@ -15,7 +15,7 @@ import os
 import mimetypes
 from decouple import Config, RepositoryEnv
 
-env=Config(RepositoryEnv('.dev.env'))
+env=Config(RepositoryEnv('.prod.env'))
 
 mimetypes.add_type("text/css", ".css", True)
 
